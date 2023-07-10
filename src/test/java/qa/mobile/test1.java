e05347b8ce09dd9354b534d19cdd7084e61d7e9a
GrabielDeLaNieve <grabieldelanieve@gmail.com>
2023-07-10 09:39:07 -0400

    First commit

---
+++ src/test/java/qa/mobile/test1.java
@@ -0,0 +1,100 @@
+package qa.mobile;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.print.DocFlavor;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class test1 {
+    AppiumDriver driver;
+
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_5");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        desiredCapabilities.setCapability("appPackage", "com.swaglabsmobileapp");
+        desiredCapabilities.setCapability("appActivity", "com.swaglabsmobileapp.SplashActivity");
+
+//        Estas dos lineas de abajo solo se ponen cuando el app no esta instalada, pero si ya lo esta se pueden comentar
+//        desiredCapabilities.setCapability("avd", "Pixel_5");
+//        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\grabiel\\IdeaProjects\\appium-pageObjectModel-2023\\src\\test\\resources\\Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
+        URL url = new URL("http://0.0.0.0:4723/wd/hub");
+
+        driver = new AndroidDriver(url, desiredCapabilities);
+        String sessionId = driver.getSessionId().toString();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void invalidUserName() {
+        WebElement usernameTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Username"));
+        WebElement passwordTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Password"));
+        WebElement loginBtn = driver.findElement(AppiumBy.accessibilityId("test-LOGIN"));
+
+        usernameTxtFld.sendKeys("incorrectusername");
+        passwordTxtFld.sendKeys("secret_sauce");
+        loginBtn.click();
+
+        WebElement errorMessage = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"test-Error message\"]/android.widget.TextView"));
+
+        String actualErrTxt = errorMessage.getAttribute("text");
+        String expectedErrTxt = "Username and password do not match any user in this service.";
+
+        Assert.assertEquals(actualErrTxt, expectedErrTxt);
+    }
+
+    @Test
+    public void invalidPassword(){
+        WebElement usernameTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Username"));
+        WebElement passwordTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Password"));
+        WebElement loginBtn = driver.findElement(AppiumBy.accessibilityId("test-LOGIN"));
+
+        usernameTxtFld.sendKeys("standard_user");
+        passwordTxtFld.sendKeys("incorrectpassword");
+        loginBtn.click();
+
+        WebElement errorMessage = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"test-Error message\"]/android.widget.TextView"));
+
+        String actualErrTxt = errorMessage.getAttribute("text");
+        String expectedErrTxt = "Username and password do not match any user in this service.";
+
+        Assert.assertEquals(actualErrTxt, expectedErrTxt);
+    }
+
+    @Test
+    public void successlogIn(){
+        WebElement usernameTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Username"));
+        WebElement passwordTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Password"));
+        WebElement loginBtn = driver.findElement(AppiumBy.accessibilityId("test-LOGIN"));
+
+        usernameTxtFld.sendKeys("standard_user");
+        passwordTxtFld.sendKeys("secret_sauce");
+        loginBtn.click();
+
+        WebElement productTitleTxt = driver.findElement(AppiumBy.xpath("//android.widget.ScrollView[@content-desc=\"test-PRODUCTS\"]/preceding-sibling::android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView"));
+        String actualProductTitle = productTitleTxt.getAttribute("text");
+        String expectedProductTitle = "PRODUCTS";
+        Assert.assertEquals(actualProductTitle, expectedProductTitle);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
